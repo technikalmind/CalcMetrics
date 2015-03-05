8881ba9ca99121c3b7d8215fe1765360281fafcc
George Hall <technikalmind@gmail.com>
2015-03-05 15:09:42 -0500

    Initial commit
    CalcMetrics computes EIGRP or OSPF metric values

---
+++ CalcMetrics/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox
+        Me.CheckBox5 = New System.Windows.Forms.CheckBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(267, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(221, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter the configured bandwidth of link (kbps):"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(167, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Calculate EIGRP"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(167, 80)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Calculate OSPF"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 68)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(39, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "K1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "EIGRP Values:"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 91)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(39, 17)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "K2"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Checked = True
+        Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 114)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(39, 17)
+        Me.CheckBox3.TabIndex = 7
+        Me.CheckBox3.Text = "K3"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(57, 68)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(39, 17)
+        Me.CheckBox4.TabIndex = 8
+        Me.CheckBox4.Text = "K4"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox5
+        '
+        Me.CheckBox5.AutoSize = True
+        Me.CheckBox5.Location = New System.Drawing.Point(57, 91)
+        Me.CheckBox5.Name = "CheckBox5"
+        Me.CheckBox5.Size = New System.Drawing.Size(39, 17)
+        Me.CheckBox5.TabIndex = 9
+        Me.CheckBox5.Text = "K5"
+        Me.CheckBox5.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Delay (usecs)"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 150)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(85, 17)
+        Me.RadioButton1.TabIndex = 14
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "FastEthernet"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 174)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(92, 17)
+        Me.RadioButton2.TabIndex = 15
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Serial/T1/56k"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 204)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.CheckBox5)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Metric Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox5 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ CalcMetrics/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Not IsNumeric(TextBox1.Text) Then
+            MsgBox("You must enter a numeric value!", Title:="Number Error", Buttons:=MsgBoxStyle.Exclamation)
+            Exit Sub
+        End If
+        Dim eigrp_Bandwidth As Integer = TextBox1.Text
+        Dim eigrp_Formula As Integer = 0
+        Dim eigrp_Result As Integer
+        Dim eigrp_Delay As Integer
+        Dim K1, K2, K3, K4, K5 As Integer
+
+        If CheckBox1.Checked = True Then
+            K1 = 1
+        Else : K1 = 0
+        End If
+
+        If CheckBox2.Checked = True Then
+            K2 = 1
+        Else : K2 = 0
+        End If
+
+        If CheckBox3.Checked = True Then
+            K3 = 1
+        Else : K3 = 0
+        End If
+
+        If CheckBox4.Checked = True Then
+            K4 = 1
+        Else : K4 = 0
+        End If
+
+        If CheckBox5.Checked = True Then
+            K5 = 1
+        Else : K5 = 0
+        End If
+
+        If RadioButton1.Checked = True Then
+            eigrp_Delay = 100
+        ElseIf RadioButton2.Checked = True Then
+            eigrp_Delay = 20000
+        End If
+
+        If K2 Or K4 Or K5 = 1 Then
+            MsgBox("Use of K2, K3, K4, and K5 no implemented yet.")
+        End If
+
+        eigrp_Bandwidth = 10 ^ 7 / eigrp_Bandwidth
+        eigrp_Delay = eigrp_Delay / 10
+        eigrp_Formula = (K1 * eigrp_Bandwidth + K3 * eigrp_Delay)
+        eigrp_Result = eigrp_Formula * 256
+        MessageBox.Show(eigrp_Result, caption:="EIGRP Metric")
+        'MessageBox.Show("K1: " & K1 & vbLf & _
+        '                "K2: " & K2 & vbLf & _
+        '                "K3: " & K3 & vbLf & _
+        '                "K4: " & K4 & vbLf & _
+        '                "K5: " & K5)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If Not IsNumeric(TextBox1.Text) Then
+            MsgBox("You must enter a numeric value!", Title:="Number Error", Buttons:=MsgBoxStyle.Exclamation)
+            Exit Sub
+        End If
+        Dim ospf_Bandwidth As Integer = TextBox1.Text * 1000
+        Dim ospf_Const As Integer = 10 ^ 8
+        Dim ospf_Metric As Integer = 0
+
+        ospf_Metric = ospf_Const / ospf_Bandwidth
+        MsgBox(ospf_Metric, Title:="OSPF Metric")
+    End Sub
+End Class
